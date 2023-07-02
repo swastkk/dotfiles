b4aa1af908806cd1f0cb12648a1e7df1bb5ee31b
Swastik Sharma <swastkk@gmail.com>
2023-07-02 17:48:48 +0530

    Minor nits in autostart
    Signed-off-by: Swastik Sharma <swastkk@gmail.com>

--- awesome/autostart.sh
+++ awesome/autostart.sh
@@ -7,10 +7,12 @@ function run {
   fi
 }
 
-run picom 
-run nitrogen --restore
+run picom -b 
+# run nitrogen --restore
 run kmix
 run nm-applet
-run xinput set-prop "DELL0A23:00 27C6:01E0 Touchpad" "libinput Tapping Enabled" 1
+# run xinput set-prop "DELL0A23:00 27C6:01E0 Touchpad" "libinput Tapping Enabled" 1
 run /home/swastik/.config/polybar/launch.sh
-# run feh --recursive --bg-fill --randomize /usr/share/backgrounds/wallpapers-2018/
+run feh --recursive --bg-fill --randomize /usr/share/backgrounds/*
+# Below is for the sake of sound in the earphones(Headphone jack)
+# run sudo dmesg | grep audio
